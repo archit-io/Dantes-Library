e7678f698006a0d20b5351ee63e87c59a4496111
Archit Choudhary <archit.digisocial@gmail.com>
2023-01-01 17:38:35 +0530

    Added main.js

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+const deleteButton = document.querySelectorAll('.del')
+const reviewItem = document.querySelectorAll('span.not')
+const reviewComplete = document.querySelectorAll('span.completed')
+
+
+Array.from(deleteButton).forEach((el)=>{
+    el.addEventListener('click', deleteReview)
+})
+
+Array.from(reviewItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(reviewComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+
+
+async function deleteReview(){
+    const reviewId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('/reviews/deleteReview', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'reviewIdFromJSFile': reviewId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
